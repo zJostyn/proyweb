84db09ee189796725f655461077beeb0dff5aee8
zJostyn <136181338+zJostyn@users.noreply.github.com>
2024-01-30 12:54:29 -0500

    Update report.component.ts

--- src/app/dashboard/report/report.component.ts
+++ src/app/dashboard/report/report.component.ts
@@ -48,12 +48,12 @@ ngOnInit(): void {
         data => this.estadoReporte = data,
         error => console.log(error));
         setTimeout(() => {
-          if(this.estadoReporte[0]?.estadoreporte == 'Abierto') {
-            this.reportes.detallesReporteU(this.id, this.infoUsuario[0].idusuario).subscribe(
+          if(this.estadoReporte[0]?.estadoreporte == 'Cerrado') {
+              this.reportes.detallesReporteUSolu(this.id, this.infoUsuario[0].idusuario).subscribe(
               data => this.reporteDetalle = data,
               error => console.log(error));
           } else {
-            this.reportes.detallesReporteUSolu(this.id, this.infoUsuario[0].idusuario).subscribe(
+              this.reportes.detallesReporteU(this.id, this.infoUsuario[0].idusuario).subscribe(
               data => this.reporteDetalle = data,
               error => console.log(error));
           }
